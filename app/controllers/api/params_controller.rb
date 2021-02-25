caf3be0468bbd616d4e380ea9797ea55b34b94a0
Lauren Miller <lomiller0913@gmail>
2021-02-24 21:07:33 -0500

    change number_guess to url segment param

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,7 +1,28 @@
 class Api::ParamsController < ApplicationController
-  def capitalize
-    @capitalize = params[:name].upcase
-    render "capitalize.json.jb"
-    #p params[:name]
+  
+  def name
+    @name = params[:name].upcase
+    if params[:name][0].upcase == "A"
+      @first_letter = "Hey, your name starts with the first letter of the alphabet!"
+    else
+      @first_letter = "Your name does not start with the first letter of the alphabet."
+    end
+    render "name.json.jb"
   end
+
+  def guess
+    @guess = params[:number_guess].to_i
+
+    if @guess > 35
+      @message = "you guessed too high"
+    elsif @guess < 35
+      @message = "you guessed too low"
+    elsif @guess == 35
+      @message = "you guessed just right"
+    end
+
+    render "guess.json.jb"
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
-  namespace :api do
-    get "/caps" => "params#capitalize"
-  end
 
   namespace :api do
-    get "/caps/:name" => "params#capitalize"
+    get "/name" => "params#name"
   end
 
-
+   namespace :api do
+    get "/guess/:number_guess" => "params#guess"
+  end
 
 end
